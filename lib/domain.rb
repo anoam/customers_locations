503bfdd6622c403e6dad5d19a8f90e0278c6195d
anoam <anoam@yandex.ru>
2018-07-05 23:04:59 +0300

    point spec

---
+++ lib/domain.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Business rules
+module Domain
+  autoload :InvalidDataError, "domain/invalid_data_error"
+  autoload :Point, "domain/point"
+
+end
---
+++ lib/domain/point.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module Domain
+  class Point
+  end
+end
