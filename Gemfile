8c070c172f5d5de98fa05bde73904cd1c67615e8
aanokhin <alexey.anokhin@surasystems.com>
2018-07-06 15:59:53 +0300

    customer repository implementation

--- Gemfile
+++ Gemfile
@@ -4,6 +4,6 @@ source "https://rubygems.org"
 
 gem "rubocop", "~> 0.57.2"
 
-gem "rspec", "~> 3.7.0"
+gem "rspec", "~> 3.7.0", require: false
 gem "yard", "0.9.14"
 gem "sinatra"
--- lib/infrastructure/customer_repository.rb
+++ lib/infrastructure/customer_repository.rb
@@ -1,8 +1,40 @@
 # frozen_string_literal: true
 
+require 'json'
 module Infrastructure
   # Storage for customers
   class CustomerRepository
 
+    # Creates repository
+    # @param file_name [String] file to load
+    # @param customer_factory [#build] factory to build {Domain::Customer}
+    # @return [Infrastructure::CustomerRepository]
+    # @raise [Domain::InvalidDataError] if given file contains invalid data
+    def self.load_file(file_name, customer_factory)
+      file_lines = File.readlines(file_name)
+
+      customers = file_lines.map do |line|
+        data = JSON.parse(line, symbolize_names: true)
+        customer_factory.build(**data)
+      end
+
+      new(customers)
+    end
+
+    # @param customers [Array<Domain::Customer>]
+    def initialize(customers)
+      @customers = customers
+    end
+
+    # Searches customers who located inside circle with center in `point` and radius `distance`
+    # @param point [Domain::Point] center of circle to search customers
+    # @param distance [Numeric] radius of circle to search customers
+    def near(point, distance)
+      customers.find_all { |customer| customer.distance_to(point) <= distance }
+    end
+
+    private
+
+    attr_reader :customers
   end
 end
