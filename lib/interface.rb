09e2ce5f0c8dec0e995cb9bbe9f629b91ca130c3
aanokhin <alexey.anokhin@surasystems.com>
2018-07-06 17:49:52 +0300

    Search form specs

---
+++ lib/interface.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# Interacting with user
+module Interface
+  autoload :SearchForm, "interface/search_form"
+end
---
+++ lib/interface/search_form.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module Interface
+  # Form object for search
+  class SearchForm
+
+  end
+end
